40605d9ed9062c5991010571efff6d55a69a7aae
LeeJaeHo-96 <towpam123@gmail.com>
2024-07-24 16:56:14 +0900

    Class 생성 1.0

---
+++ Class_이재호/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        class Character
+        {
+            public Character level;
+            public Character Hp;
+            public Character moveSpeed;
+            public Character atk;
+
+            void moveForward()
+            {
+                Console.WriteLine("앞으로 이동합니다.");
+            }
+            void moveBackward()
+            {
+                Console.WriteLine("뒤로 이동합니다.");
+            }
+            void leftside()
+            {
+                Console.WriteLine("90도 좌회전합니다.");
+            }
+            void rightside()
+            {
+                Console.WriteLine("90도 우회전합니다.");
+            }
+            void attack()
+            {
+                Console.WriteLine("공격합니다.");
+            }
+            void hit()
+            {
+                Console.WriteLine("피격 당했습니다");
+            }
+
+            class Monser : Character
+            {
+
+            }
+
+            class PlayerCharacter : Character
+            {
+
+            }
+
+            class PlayerManagement : Character
+            {
+
+            }
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
